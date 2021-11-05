8f7cb65649da4f5ea4ac1752de5c4c42d7c1df65
lidaning <453882101@qq.com>
2021-11-05 17:16:18 +0800

    添加配置中心

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # mySpringcloud-netflix
+
 It's a springcloud application which is base on netflix
 
 ## Integrate eureka
+
+集成服务发现, eureka模块作为服务发现,其它服务实现eureka client配置就可以了, 添加依赖, 启动类添加注解
+
+1. eureka.client.service-url.defaultZone=http://localhost:8761/eureka/
+
+## Integrate config
+
+集成配置中心,像上述的服务发现的配置,就可以统一通过配置中心自动加载
--- config/src/main/java/com/lidaning/config/ConfigApplication.java
+++ config/src/main/java/com/lidaning/config/ConfigApplication.java
@@ -1,15 +1,17 @@
-package com.lidaning.demo;
+package com.lidaning.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
+@EnableConfigServer
 @SpringBootApplication
-public class DemoApplication {
+public class ConfigApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(ConfigApplication.class, args);
     }
 
 }
