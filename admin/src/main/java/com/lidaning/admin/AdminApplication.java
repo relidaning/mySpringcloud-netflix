cc65d7c55c031fb1446d046f3129482b4480923f
lidaning <453882101@qq.com>
2021-11-05 23:53:17 +0800

    添加springboot-admin监控中心; demo项目添加actuator组件;

--- admin/src/main/java/com/lidaning/admin/AdminApplication.java
+++ admin/src/main/java/com/lidaning/admin/AdminApplication.java
@@ -1,8 +1,12 @@
 package com.lidaning.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableAdminServer
+@EnableEurekaClient
 @SpringBootApplication
 public class AdminApplication {
 
